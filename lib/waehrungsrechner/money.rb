f7987b2268a1ca74ddc42d19bfe714c276367fd3
Dan Kreiger <dan@dankreiger.com>
2017-04-25 03:42:01 +0200

    Experiment with downgrade

--- lib/waehrungsrechner/money.rb
+++ lib/waehrungsrechner/money.rb
@@ -4,6 +4,7 @@ class Money
   attr_reader :amount, :base_currency, :rates
 
   def initialize amount, base_currency
+    raise ArgumentError, "Amount must be a number" unless amount.is_a? Numeric
     raise ArgumentError, " `#{base_currency}` is invalid (currency symbol must be a 3 character string)`#{base_currency}` is invalid." unless valid_currency? base_currency
     begin
       @rate_info = HTTParty.get "http://api.fixer.io/latest?base=#{base_currency}"
@@ -12,20 +13,39 @@ class Money
       puts "Something went wrong. Please make sure are connected to the internet."
       puts e.inspect
     end
-
+    base_currency.upcase!
     @amount = amount
     @base_currency = base_currency
   end
 
   def inspect
-    "#{'%.2f' % amount} #{base_currency.upcase}"
+    "#{('%.2f' % amount)} #{base_currency.upcase}"
   end
 
   def convert_to transfer_currency
     raise ArgumentError, "`#{transfer_currency}` must be a 3 character string" unless transfer_currency.is_a?(String) && transfer_currency.length == 3
     raise ArgumentError, "`#{transfer_currency}` is either an invalid currency symbol, or it is not currently supported." unless (rates = @rate_info['rates']) && rates.keys.include?(transfer_currency)
 
-    amount * rates[transfer_currency]
+    Money.new(amount * rates[transfer_currency], transfer_currency)
+  end
+
+  # adding money objects together
+  def +(other)
+    if base_currency == other.base_currency
+      Money.new(amount + other.amount, base_currency)
+    else
+      # maybe ask the user?
+      puts "Which currency would you like to see the total in?\n1.#{base_currency}\n2.#{other.base_currency}"
+      answer = gets.chomp
+      case answer.upcase
+      # calculate the total in the base currency
+      when '1', "#{base_currency}"
+        Money.new(amount + other.convert_to(base_currency), base_currency)
+        # calculate the total in the transfer currency
+      when '2', "#{other.base_currency}"
+        Money.new(self.convert_to(other.base_currency) + other.amount, other.base_currency)
+      end
+    end
   end
 
   private
--- spec/waehrungsrechner/money_spec.rb
+++ spec/waehrungsrechner/money_spec.rb
@@ -1,24 +1,42 @@
 require "spec_helper"
+require "stringio"
 
 describe Money do
-  let(:amount)   { 50 }
-  let(:currency) { 'EUR' }
-  let(:money)    { Money.new(amount, currency) }
-  let(:rates)    { HTTParty.get("http://api.fixer.io/latest?base=#{currency}")['rates'] }
+  let(:money_euro)   { Money.new(50, 'EUR') }
+  let(:money_euro2)  { Money.new(60, 'EUR') }
+  let(:money_usd)    { Money.new(45, 'USD') }
+  let(:rates)        { fetch_rates('EUR') }
 
-  it "returns the inputted amount" do
-    expect(money.amount).to eq 50
+  it "returns an inputted amount" do
+    expect(money_euro.amount).to eq 50
+  end
+
+  it "does not accept non-numericals for the amount" do
+    expect{Money.new('50 hi there', 'EUR')}.to raise_error ArgumentError, 'Amount must be a number'
   end
 
   it "returns the inputted currency" do
-    expect(money.base_currency).to eq 'EUR'
+    expect(money_euro.base_currency).to eq 'EUR'
   end
 
   it "returns a readable string with the amount and currency" do
-    expect(money.inspect).to eq '50.00 EUR'
+    expect(money_euro.inspect).to eq('50.00 EUR')
   end
 
   it "converts the given amount and currency to a newly inputted currency" do
-    expect(money.convert_to('USD')).to eq amount * rates['USD']
+    expect(money_euro.convert_to('USD').inspect).to eq Money.new(50 * rates['USD'], 'USD').inspect
+  end
+
+  it "adds two money objects together" do
+    expect(money_euro + money_euro2).to eq (money_euro.amount + money_euro2.amount), 'EUR'
+    $stdin = StringIO.new("1")
+    expect(money_euro + money_usd).to eq (money_euro.amount + money_usd.convert_to('EUR').amount)
+    $stdin = StringIO.new("2")
+    expect(money_euro + money_usd).to eq (money_euro.convert_to('USD').amount + money_usd.amount)
+  end
+
+
+  def fetch_rates(base_currency)
+    HTTParty.get("http://api.fixer.io/latest?base=#{base_currency}")['rates']
   end
 end
